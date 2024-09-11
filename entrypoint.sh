d6d4fe266040ebb9682350b6983bee35cd83c15a
user.name <marcorandazzo051291@gmail.com>
2024-09-12 00:26:44 +0200

    nginx entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,77 @@
 #!/bin/bash
 cp /root/nginx.conf /etc/nginx/nginx.conf
-service nginx restart &
+# service nginx restart &
 
-if [ "$server" = "start" ]; then 
-    export DJANGO_SETTINGS_MODULE=config.settings
-    gunicorn config.wsgi:application \
-    --limit-request-line 0 \
-    --limit-request-fields 512 \
-    --limit-request-field_size 32000 \
-    --name ludwing2 \
-    --workers 2 \
-    --worker-class gthread  \
-    --log-level=info \
-    --log-file=- \
-    --timeout 1800 \
-    -b 0.0.0.0:3001
+ORIGIN="$2"
+
+if [ "$1" == 'start' ]; then
+
+    if [ "$#" -ge 3 ]; then
+        DOMAIN="$3"
+        sed -i "s/SERVERNAME/$DOMAIN/g" /etc/nginx/nginx.conf  
+        exec service nginx start &
+        export DJANGO_SETTINGS_MODULE=config.settings
+        gunicorn config.wsgi:application \
+        --limit-request-line 0 \
+        --limit-request-fields 512 \
+        --limit-request-field_size 32000 \
+        --name ludwing2 \
+        --workers 2 \
+        --worker-class gthread  \
+        --log-level=info \
+        --log-file=- \
+        --timeout 1800 \
+        -b 0.0.0.0:3001 
+    else
+        export DJANGO_SETTINGS_MODULE=config.settings
+        gunicorn config.wsgi:application \
+        --limit-request-line 0 \
+        --limit-request-fields 512 \
+        --limit-request-field_size 32000 \
+        --name ludwing2 \
+        --workers 2 \
+        --worker-class gthread  \
+        --log-level=info \
+        --log-file=- \
+        --timeout 1800 \
+        -b 0.0.0.0:3001 &    
+    fi
+
+    # # node init
+    # export ORIGIN="$ORIGIN" 
+
+    # cd /app
+    # rm -rf dist
+    # npm i
+    # npm install react-icons
+    # npm run build
+    # npm install -g serve
+
+    # if [ "$#" -ge 3 ]; then
+    #     DOMAIN="$3"
+    #     sed -i "s/SERVERNAME/$DOMAIN/g" /etc/nginx/nginx.conf  
+    #     exec service nginx start &
+    #     serve -s dist 
+    # else
+    #     serve -s dist     
+    # fi
+
 fi
+
+# if [ "$server" = "start" ]; then 
+#     export DJANGO_SETTINGS_MODULE=config.settings
+#     gunicorn config.wsgi:application \
+#     --limit-request-line 0 \
+#     --limit-request-fields 512 \
+#     --limit-request-field_size 32000 \
+#     --name ludwing2 \
+#     --workers 2 \
+#     --worker-class gthread  \
+#     --log-level=info \
+#     --log-file=- \
+#     --timeout 1800 \
+#     -b 0.0.0.0:3001
+# fi
 
 if [ "$1" == 'bash' ]; then
     /bin/bash
